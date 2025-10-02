f0837a17cbf20858083d4f8f437ec39f07af24dc
Alex-Vieira-da-Silva <alexvieira.360@hotmail.com>
2025-10-02 19:25:42 -0300

    iniciando projeto

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+namespace MinimalApi.DTOs
+{
+    public class LogindDTO
+    {
+        public string Email { get; set; } = default!;
+        public string Senha { get; set; } = default!;
+    }
+}
---
+++ Infraestrutura/Db/DbContexto.cs
@@ -0,0 +1,7 @@
+namespace Minimalapi.Infraestrutura.Db;
+
+public class DbContexto
+{
+    
+}
+
